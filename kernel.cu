2c2cdde690ccbc6894e57418fb62d483bbe144da
Kyjko <Kyjko@users.noreply.github.com>
2021-01-26 21:36:42 +0100

    Update kernel.cu

--- kernel.cu
+++ kernel.cu
@@ -1,4 +1,4 @@
-﻿/***
+/***
 
 	Simple Cellular Automata based sandbox simulation by Miki
 
@@ -17,6 +17,8 @@
 #include <memory.h>
 #include <stdbool.h>
 
+#include "Pixelworld.h"
+
 /*
 	air:	0x00, 0x00, 0x00 (black)
 	water:	0x00, 0x00, 0xff (blue)
@@ -24,17 +26,13 @@
 	metal:	0x77, 0x77, 0x77 (grey)
 */
 
-#define W 1024
-#define H 1024
-#define BYTES_PER_COLOR 3
-
-#define UNDEFINED 0
-#define AIR 1
-#define SAND 2
-#define WATER 3
-#define METAL 4
-
-#define IN_BOUNDS(x, y) (x > 0 && x < W && y > 0 && y < H)
+void inline gpuAssert(cudaError_t code, const char* file, int line, bool abort = true) {
+	if (code != cudaSuccess) {
+		fprintf(stderr, "GPUAssert: %s %s %d\n", cudaGetErrorString(code), file, line);
+		if (abort)
+			exit(code);
+	}
+}
 
 static int RADIUS = 1;
 static int MOUSEX = 0, MOUSEY = 0;
@@ -43,50 +41,15 @@ static SDL_Window* w;
 static SDL_GLContext glctx;
 static bool quit = false;
 
-__host__ __device__ unsigned int inline color_to_type(unsigned char r, unsigned char g, unsigned char b) {
-	if (r == 0xFF && g == 0xFF && b == 0x00) {
-		//sand
-		return 2;
-	}
-	else if (r == 0x00 && g == 0x00 && b == 0xFF) {
-		//water
-		return 3;
-	}
-	else if (r == 0x00 && g == 0x00 && b == 0x00) {
-		//air
-		return 1;
-	}
-	else if (r == 0x77 && g == 0x77 && b == 0x77) {
-		//metal
-		return 4;
-	}
-	else {
-		//undefined
-		return 0;
-	}
+__host__ __device__ __declspec(noalias) unsigned int inline color_to_type(unsigned char r, unsigned char g, unsigned char b) {
+	//never return undefined
+	return (r == 0xFF && g == 0xFF && b == 0x00) * SAND + (r == 0x00 && g == 0x00 && b == 0xFF) * WATER
+		+ (r == 0x00 && g == 0x00 && b == 0x00) * AIR + (r == 0x77 && g == 0x77 && b == 0x77) * METAL;
 }
 
-__device__ int inline type_to_color(unsigned int t) {
-	if (t == 1) {
-		//air
-		return 0x000000;
-	}
-	else if(t == 2) {
-		//sand
-		return 0xFFFF00;
-	}
-	else if (t == 3) {
-		//water
-		return 0x0000FF;
-	}
-	else if (t == 4) {
-		//metal
-		return 0x777777;
-	}
-	else {
-		//unhandled undefined
-		return 0x000000;
-	}
+__device__ __declspec(noalias) int inline type_to_color(unsigned int t) {
+	//returns undefined
+	return (t == 1) * 0x000000 + (t == 2) * 0xFFFF00 + (t == 3) * 0x0000FF + (t == 4) * 0x777777 + (t == 0) * 0x000000;
 }
 
 __global__ void kernel(unsigned char* pixels, unsigned char* buffer_pixels) {
@@ -97,11 +60,9 @@ __global__ void kernel(unsigned char* pixels, unsigned char* buffer_pixels) {
 
 	int x = idx % W;
 	int y = (int)(idx / W);
-	
-	// color of current pixel
-	unsigned char r = pixels[(y*W + x)*3];
-	unsigned char g = pixels[(y*W + x) * 3 + 1];
-	unsigned char b = pixels[(y*W + x) * 3 + 2];
+
+	unsigned char r, g, b;
+	GETPIXEL(pixels, r, g, b, x, y);
 
 	unsigned int type = color_to_type(r, g, b);
 	
@@ -113,9 +74,8 @@ __global__ void kernel(unsigned char* pixels, unsigned char* buffer_pixels) {
 
 		// test pixels below
 		for (int i = -1; i <= 1; i++) {
-			unsigned char n_r = pixels[((y - 1)*W + (x + i)) * 3];
-			unsigned char n_g = pixels[((y - 1)*W + (x + i)) * 3 + 1];
-			unsigned char n_b = pixels[((y - 1)*W + (x + i)) * 3 + 2];
+			unsigned char n_r, n_g, n_b;
+			GETPIXEL(pixels, n_r, n_g, n_b, x + i, y - 1);
 
 			unsigned int n_type = color_to_type(n_r, n_g, n_b);
 
@@ -125,48 +85,34 @@ __global__ void kernel(unsigned char* pixels, unsigned char* buffer_pixels) {
 
 		// bottom is free => flow to bottom left => sand flows down
 		if (!nbs[1]) {
-			buffer_pixels[((y - 1)*W + (x)) * 3] = 0xFF;
-			buffer_pixels[((y - 1)*W + (x)) * 3 + 1] = 0xFF;
-			buffer_pixels[((y - 1)*W + (x)) * 3 + 2] = 0x00;
 
-			buffer_pixels[((y)*W + (x)) * 3] = 0x00;
-			buffer_pixels[((y)*W + (x)) * 3 + 1] = 0x00;
-			buffer_pixels[((y)*W + (x)) * 3 + 2] = 0x00;
-		} 
+			SETPIXEL(buffer_pixels, x, y - 1, SAND);
+			SETPIXEL(buffer_pixels, x, y, AIR);
+		}
 		// bottom is occupied, but left is empty => sand flows down left
 		else if (nbs[1] && !nbs[0]) {
-			buffer_pixels[((y - 1)*W + (x - 1)) * 3] = 0xFF;
-			buffer_pixels[((y - 1)*W + (x - 1)) * 3 + 1] = 0xFF;
-			buffer_pixels[((y - 1)*W + (x - 1)) * 3 + 2] = 0x00;
 
-			buffer_pixels[((y)*W + (x)) * 3] = 0x00;
-			buffer_pixels[((y)*W + (x)) * 3 + 1] = 0x00;
-			buffer_pixels[((y)*W + (x)) * 3 + 2] = 0x00;
+			SETPIXEL(buffer_pixels, x - 1, y - 1, SAND);
+			SETPIXEL(buffer_pixels, x, y, AIR);
 		} 
 		// only bottom right is free => flow down right
 		else if (nbs[1] && nbs[0] && !nbs[2]) {
-			buffer_pixels[((y - 1)*W + (x + 1)) * 3] = 0xFF;
-			buffer_pixels[((y - 1)*W + (x + 1)) * 3 + 1] = 0xFF;
-			buffer_pixels[((y - 1)*W + (x + 1)) * 3 + 2] = 0x00;
 
-			buffer_pixels[((y)*W + (x)) * 3] = 0x00;
-			buffer_pixels[((y)*W + (x)) * 3 + 1] = 0x00;
-			buffer_pixels[((y)*W + (x)) * 3 + 2] = 0x00;
+			SETPIXEL(buffer_pixels, x + 1, y - 1, SAND);
+			SETPIXEL(buffer_pixels, x, y, AIR);
 		} 
 		// none of bottom pixels are free => stay put
 		else if (nbs[1] && nbs[0] && nbs[2]) {
-			buffer_pixels[((y)*W + (x)) * 3] = 0xFF;
-			buffer_pixels[((y)*W + (x)) * 3 + 1] = 0xFF;
-			buffer_pixels[((y)*W + (x)) * 3 + 2] = 0x00;
+
+			SETPIXEL(buffer_pixels, x, y, SAND);
 		}
 	}
 	// for metal blocks, they always stay put
 	else if (type == METAL && IN_BOUNDS(x, y)) {
-		buffer_pixels[((y)*W + (x)) * 3] = 0x77;
-		buffer_pixels[((y)*W + (x)) * 3 + 1] = 0x77;
-		buffer_pixels[((y)*W + (x)) * 3 + 2] = 0x77;
+
+		SETPIXEL(buffer_pixels, x, y, METAL);
 	}
-	// water behaves just like sand, except it spreads horizontally
+	// water behaves just like sand, except it spreads horizontally as well
 	else if (type == WATER && IN_BOUNDS(x, y)) {
 
 		// 0th: bottom left, 1st: bottom, 2nd: bottom right, 3rd: left, 4th: right
@@ -174,107 +120,64 @@ __global__ void kernel(unsigned char* pixels, unsigned char* buffer_pixels) {
 
 		// test pixels below
 		for (int i = -1; i <= 1; i++) {
-			unsigned char n_r = pixels[((y - 1)*W + (x + i)) * 3];
-			unsigned char n_g = pixels[((y - 1)*W + (x + i)) * 3 + 1];
-			unsigned char n_b = pixels[((y - 1)*W + (x + i)) * 3 + 2];
+			unsigned char n_r, n_g, n_b;
+			GETPIXEL(pixels, n_r, n_g, n_b, x + i, y - 1);
 
 			unsigned int n_type = color_to_type(n_r, n_g, n_b);
 
-			if (n_type == SAND || n_type == METAL || n_type == WATER)
+			if (n_type == METAL || n_type == WATER)
 				nbs[i + 1] = 1;
 		}
 
-		// test left and right pixels
-		unsigned char r_r = pixels[((y)*W + (x + 1)) * 3];
-		unsigned char r_g = pixels[((y)*W + (x + 1)) * 3 + 1];
-		unsigned char r_b = pixels[((y)*W + (x + 1)) * 3 + 2];
+		unsigned char r_r, r_g, r_b;
+		GETPIXEL(pixels, r_r, r_g, r_b, x + 1, y);
 
 		unsigned int r_type = color_to_type(r_r, r_g, r_b);
-		if (r_type == SAND || r_type == METAL || r_type == WATER)
+		if (r_type == METAL || r_type == WATER)
 			nbs[4] = 1;
 
-		unsigned char l_r = pixels[((y)*W + (x - 1)) * 3];
-		unsigned char l_g = pixels[((y)*W + (x - 1)) * 3 + 1];
-		unsigned char l_b = pixels[((y)*W + (x - 1)) * 3 + 2];
+		unsigned char l_r, l_g, l_b;
+		GETPIXEL(pixels, l_r, l_g, l_b, x - 1, y);
 
 		unsigned int l_type = color_to_type(l_r, l_g, l_b);
-		if (l_type == SAND || l_type == METAL || l_type == WATER)
+		if (l_type == METAL || l_type == WATER)
 			nbs[3] = 1;
 
-
 		if (!nbs[1]) {
-			buffer_pixels[((y - 1)*W + (x)) * 3] = 0x00;
-			buffer_pixels[((y - 1)*W + (x)) * 3 + 1] = 0x00;
-			buffer_pixels[((y - 1)*W + (x)) * 3 + 2] = 0xFF;
 
-			buffer_pixels[((y)*W + (x)) * 3] = 0x00;
-			buffer_pixels[((y)*W + (x)) * 3 + 1] = 0x00;
-			buffer_pixels[((y)*W + (x)) * 3 + 2] = 0x00;
+			SETPIXEL(buffer_pixels, x, y - 1, WATER);
+			SETPIXEL(buffer_pixels, x, y, AIR);
 		}
 		// bottom is occupied, but left is empty => water flows down left
 		else if (nbs[1] && !nbs[0]) {
-			buffer_pixels[((y - 1)*W + (x - 1)) * 3] = 0x00;
-			buffer_pixels[((y - 1)*W + (x - 1)) * 3 + 1] = 0x00;
-			buffer_pixels[((y - 1)*W + (x - 1)) * 3 + 2] = 0xFF;
 
-			buffer_pixels[((y)*W + (x)) * 3] = 0x00;
-			buffer_pixels[((y)*W + (x)) * 3 + 1] = 0x00;
-			buffer_pixels[((y)*W + (x)) * 3 + 2] = 0x00;
+			SETPIXEL(buffer_pixels, x - 1, y - 1, WATER);
+			SETPIXEL(buffer_pixels, x, y, AIR);
 		}
 		// only bottom right is free => flow down right
 		else if (nbs[1] && nbs[0] && !nbs[2]) {
-			buffer_pixels[((y - 1)*W + (x + 1)) * 3] = 0x00;
-			buffer_pixels[((y - 1)*W + (x + 1)) * 3 + 1] = 0x00;
-			buffer_pixels[((y - 1)*W + (x + 1)) * 3 + 2] = 0xFF;
 
-			buffer_pixels[((y)*W + (x)) * 3] = 0x00;
-			buffer_pixels[((y)*W + (x)) * 3 + 1] = 0x00;
-			buffer_pixels[((y)*W + (x)) * 3 + 2] = 0x00;
+			SETPIXEL(buffer_pixels, x + 1, y - 1, WATER);
+			SETPIXEL(buffer_pixels, x, y, AIR);
 		}
-		// none of bottom pixels are free => look for sides
+		// no bottom pixels available
 		else if (nbs[1] && nbs[0] && nbs[2]) {
-			// if left is empty
+			// flow left
 			if (!nbs[3]) {
-				buffer_pixels[((y)*W + (x - 1)) * 3] = 0x00;
-				buffer_pixels[((y)*W + (x - 1)) * 3 + 1] = 0x00;
-				buffer_pixels[((y)*W + (x - 1)) * 3 + 2] = 0xFF;
-
-				buffer_pixels[((y)*W + (x)) * 3] = 0x00;
-				buffer_pixels[((y)*W + (x)) * 3 + 1] = 0x00;
-				buffer_pixels[((y)*W + (x)) * 3 + 2] = 0x00;
-
-			// if left is occupied, but right is empty
+				SETPIXEL(buffer_pixels, x - 1, y, WATER);
+			
 			}
-			else if (nbs[3] && !nbs[4]) {
-				buffer_pixels[((y)*W + (x + 1)) * 3] = 0x00;
-				buffer_pixels[((y)*W + (x + 1)) * 3 + 1] = 0x00;
-				buffer_pixels[((y)*W + (x + 1)) * 3 + 2] = 0xFF;
-
-				buffer_pixels[((y)*W + (x)) * 3] = 0x00;
-				buffer_pixels[((y)*W + (x)) * 3 + 1] = 0x00;
-				buffer_pixels[((y)*W + (x)) * 3 + 2] = 0x00;
-			} 
-			// both direction
-			else if (!nbs[3] && !nbs[4]) {
-				buffer_pixels[((y)*W + (x - 1)) * 3] = 0x00;
-				buffer_pixels[((y)*W + (x - 1)) * 3 + 1] = 0x00;
-				buffer_pixels[((y)*W + (x - 1)) * 3 + 2] = 0xFF;
-
-				buffer_pixels[((y)*W + (x + 1)) * 3] = 0x00;
-				buffer_pixels[((y)*W + (x + 1)) * 3 + 1] = 0x00;
-				buffer_pixels[((y)*W + (x + 1)) * 3 + 2] = 0xFF;
-
-				buffer_pixels[((y)*W + (x)) * 3] = 0x00;
-				buffer_pixels[((y)*W + (x)) * 3 + 1] = 0x00;
-				buffer_pixels[((y)*W + (x)) * 3 + 2] = 0x00;
+			if (!nbs[4]) {
+				SETPIXEL(buffer_pixels, x + 1, y, WATER);
 			}
-			// neither => stay put
-			else {
-				buffer_pixels[((y)*W + (x)) * 3] = 0x00;
-				buffer_pixels[((y)*W + (x)) * 3 + 1] = 0x00;
-				buffer_pixels[((y)*W + (x)) * 3 + 2] = 0xFF;
+			SETPIXEL(buffer_pixels, x, y, AIR);
+
+			if (nbs[3] && nbs[4]) {
+				SETPIXEL(buffer_pixels, x, y, WATER);
 			}
+
 		}
+		
 	}
 }
 
@@ -287,14 +190,11 @@ __global__ void kernel_update_pixels_buffer(unsigned char* pixels, unsigned char
 	int x = idx % W;
 	int y = (int)(idx / W);
 
-	pixels[((y)*W + (x)) * 3] = buffer_pixels[((y)*W + (x)) * 3];
-	pixels[((y)*W + (x)) * 3 + 1] = buffer_pixels[((y)*W + (x)) * 3 + 1];
-	pixels[((y)*W + (x)) * 3 + 2] = buffer_pixels[((y)*W + (x)) * 3 + 2];
+	UPDATEPIXEL(pixels, buffer_pixels, x, y);
 
 	if (IN_BOUNDS(x, y)) {
-		buffer_pixels[((y)*W + (x)) * 3] = 0x00;
-		buffer_pixels[((y)*W + (x)) * 3 + 1] = 0x00;
-		buffer_pixels[((y)*W + (x)) * 3 + 2] = 0x00;
+
+		SETPIXEL(buffer_pixels, x, y, AIR);
 	}
 }
 
@@ -322,7 +222,7 @@ errno_t Init() {
 	return 0;
 }
 
-void DrawCircle(float cx, float cy, float r, int num_segments) {
+__declspec(noalias) void DrawCircle(float cx, float cy, float r, int num_segments) {
 	glBegin(GL_LINE_LOOP);
 	for (int ii = 0; ii < num_segments; ii++) {
 		float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);
@@ -334,14 +234,17 @@ void DrawCircle(float cx, float cy, float r, int num_segments) {
 }
 
 void Render(unsigned char* pixels, unsigned char* d_pixels, unsigned char* d_buffer_pixels) {
-	cudaMemcpy(d_pixels, pixels, W*H*BYTES_PER_COLOR * sizeof(char), cudaMemcpyHostToDevice);
 	
-	kernel <<<W, H>>> (d_pixels, d_buffer_pixels);
-	kernel_update_pixels_buffer <<<W, H>>> (d_pixels, d_buffer_pixels);
+	gpuErrorCheck(cudaMemcpy(d_pixels, pixels, W*H*BYTES_PER_COLOR * sizeof(char), cudaMemcpyHostToDevice));
 	
+	kernel <<<N_BLOCKS, N_THREADS>>> (d_pixels, d_buffer_pixels);
+	gpuErrorCheck(cudaPeekAtLastError());
+	kernel_update_pixels_buffer <<<N_BLOCKS, N_THREADS>>> (d_pixels, d_buffer_pixels);
+	gpuErrorCheck(cudaPeekAtLastError());
+
 	cudaDeviceSynchronize();
 
-	cudaMemcpy(pixels, d_pixels, W*H * BYTES_PER_COLOR * sizeof(char), cudaMemcpyDeviceToHost);
+	gpuErrorCheck(cudaMemcpy(pixels, d_pixels, W*H * BYTES_PER_COLOR * sizeof(char), cudaMemcpyDeviceToHost));
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -359,8 +262,8 @@ void Start() {
 	unsigned char* d_pixels;
 	unsigned char* d_buffer_pixels;
 
-	cudaMalloc((void**)&d_pixels, BYTES_PER_COLOR*W*H * sizeof(char));
-	cudaMalloc((void**)&d_buffer_pixels, BYTES_PER_COLOR*W*H * sizeof(char));
+	gpuErrorCheck(cudaMalloc((void**)&d_pixels, BYTES_PER_COLOR*W*H * sizeof(char)));
+	gpuErrorCheck(cudaMalloc((void**)&d_buffer_pixels, BYTES_PER_COLOR*W*H * sizeof(char)));
 
 	// initialize pixels array
 	memset(pixels, 0x00, W*H*BYTES_PER_COLOR);
@@ -418,6 +321,7 @@ void Start() {
 			case SDL_MOUSEMOTION:
 				MOUSEX = e.motion.x;
 				MOUSEY = e.motion.y;
+				if (IN_BOUNDS(MOUSEX, MOUSEY)) {
 					if (leftbuttondown) {
 						int mx = e.motion.x;
 						int my = e.motion.y;
@@ -465,9 +369,9 @@ void Start() {
 							for (int j = 0; j < H; j++) {
 								if ((i - mx)*(i - mx) + (j - my)*(j - my) <= RADIUS * RADIUS) {
 									//erase only non-air pixels
-									unsigned int type = color_to_type(pixels[((H-j)*W + i) * 3], 
-												pixels[((H - j)*W + i) * 3 + 1], pixels[((H - j)*W + i) * 3 + 2]);
-									
+									unsigned int type = color_to_type(pixels[((H - j)*W + i) * 3],
+										pixels[((H - j)*W + i) * 3 + 1], pixels[((H - j)*W + i) * 3 + 2]);
+
 									if (type == METAL || type == SAND || type == WATER) {
 										pixels[((H - j)*W + i) * 3] = 0x00;
 										pixels[((H - j)*W + i) * 3 + 1] = 0x00;
@@ -498,8 +402,8 @@ void Start() {
 						}
 						//break;
 					}
-				
 
+				}
 			default: {}
 			}
 		}
@@ -524,13 +428,13 @@ int main(int argc, char** argv) {
 
 	errno_t err = Init();
 	if (err < 0) {
-		fprintf(stderr, "[-] Error: %d\n", err);
+		ERRLOG("[-] Error in Init");
 		exit(-1);
 	}
 
-	fprintf(stdout, "[+] Starting\n");
+	LOG("[+] Starting");
 	Start();
-	fprintf(stdout, "[-] Quitting\n");
+	LOG("[-] Quitting");
 
 	return 0;
 }
